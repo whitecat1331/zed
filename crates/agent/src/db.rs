aa25652d948cd6365bd5331237f542ef47d64be5
Patrick Loyd <patrickloyd789@gmail.com>
2026-09-17 21:59:38 -0400

    agent: Fix reconcile updated_at error conversion

--- crates/agent/src/db.rs
+++ crates/agent/src/db.rs
@@ -1044,7 +1044,7 @@ fn reconcile_threads_sync(
             let max_updated_at = members
                 .iter()
                 .map(|member| DateTime::parse_from_rfc3339(&member.updated_at))
-                .collect::<Result<Vec<_>>>()?
+                .collect::<Result<Vec<_>, chrono::ParseError>>()?
                 .into_iter()
                 .map(|updated_at| updated_at.with_timezone(&Utc))
                 .max()
